84d1e9aedd10982282532bceae2585c4d68fee51
zcd <johnson846440403@gmail.com>
2026-03-16 15:48:19 +0800

    finish proj1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -2,7 +2,7 @@ package deque;
 
 import java.util.Iterator;
 
-public class ArrayDeque<T> implements Deque<T> {
+public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
     private int size;
     private T[] items;
     private int nextFirst;
